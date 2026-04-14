3e41e3d626bf1fb408503e76bc05dbd82e5bc638
Ashraful Rifat <106437747+ashrafulRifat@users.noreply.github.com>
2026-04-14 15:07:45 +0600

    Delete agri_inventory_management directory

--- agri_inventory_management/auth/logout.php
+++
@@ -1,8 +0,0 @@
-<?php
-declare(strict_types=1);
-
-require_once __DIR__ . '/../includes/bootstrap.php';
-
-logout_user();
-
-redirect('/agri_inventory_management/auth/login.php');
--- agri_inventory_management/includes/footer.php
+++
@@ -1,6 +0,0 @@
-        </main>
-    </div>
-</div>
-<script src="/agri_inventory_management/assets/js/app.js"></script>
-</body>
-</html>
